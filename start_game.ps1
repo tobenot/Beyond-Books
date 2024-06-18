89fba135d5abcc0e7ee606791dc5cc2cf3a78cf9
tobenot <2818373670@qq.com>
2024-06-19 03:27:17 +0800

    feat: 更新启动脚本，增加重启功能

--- start_game.ps1
+++ start_game.ps1
@@ -4,7 +4,29 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 # 设置端口
 $Port = 8000
 
-Write-Host "Starting HTTP server on port $Port"
+function Start-HttpServer {
+    param ($Port, $currentDir)
+    Write-Host "Starting HTTP server on port $Port"
+    
+    # 启动Python的HTTP服务器作为后台任务
+    $job = Start-Job -ScriptBlock {
+        param ($Port, $currentDir)
+        
+        try {
+            Set-Location $currentDir
+            python -m http.server $Port
+        } catch {
+            $_ | Write-Host
+        }
+    } -ArgumentList $Port, $currentDir
+    
+    Start-Sleep -Seconds 1 # 等待服务器启动
+
+    # 打开默认浏览器并跳转到本地服务器网址
+    Start-Process "http://localhost:$Port"
+
+    return $job
+}
 
 # 切换到当前目录
 try {
@@ -15,25 +37,11 @@ try {
     exit
 }
 
-# 启动Python的HTTP服务器作为后台任务
-$job = Start-Job -ScriptBlock {
-    param ($Port, $currentDir)
-    
-    try {
-        Set-Location $currentDir
-        python -m http.server $Port
-    } catch {
-        $_ | Write-Host
-    }
-} -ArgumentList $Port, $currentDir
+# 启动初始的HTTP服务器
+$job = Start-HttpServer -Port $Port -currentDir $currentDir
 
-Start-Sleep -Seconds 1 # 等待服务器启动
-
-# 打开默认浏览器并跳转到本地服务器网址
-Start-Process "http://localhost:$Port"
-
-# 使用用户输入来停止服务器
-Write-Host "Press 'q' to stop the server..."
+# 使用用户输入来停止或重启服务器
+Write-Host "Press 'q' to stop the server or 'r' to restart the server..."
 while ($true) {
     $input = Read-Host
     if ($input -eq 'q') {
@@ -42,5 +50,10 @@ while ($true) {
         Remove-Job -Id $job.Id
         Write-Host "HTTP server stopped."
         break
+    } elseif ($input -eq 'r') {
+        Write-Host "Restarting HTTP server..."
+        Stop-Job -Id $job.Id
+        Remove-Job -Id $job.Id
+        $job = Start-HttpServer -Port $Port -currentDir $currentDir
     }
 }
