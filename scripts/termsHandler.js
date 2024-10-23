0d62e953533d997351d7ade6012c0f923d70fc0f
tobenot <2818373670@qq.com>
2024-10-24 01:32:19 +0800

    bugfix: load config

--- scripts/termsHandler.js
+++ scripts/termsHandler.js
@@ -1,9 +1,11 @@
 let termsConfig = {};
 let colorsConfig = {};
 
+const BASE_URL = process.env.VUE_APP_BASE_URL || '';
+
 async function loadTermsConfig() {
     try {
-        const response = await fetch('lang/terms_explanations_zh-CN.json?v=' + new Date().getTime());
+        const response = await fetch(`/assets/config/terms_explanations_zh-CN.json?v=${new Date().getTime()}`);
         termsConfig = await response.json();
         if (isCarrotTest()) console.log('Loaded terms config:', termsConfig);
     } catch (error) {
@@ -14,7 +16,7 @@ async function loadTermsConfig() {
 
 async function loadColorsConfig() {
     try {
-        const response = await fetch('config/colors.json?v=' + new Date().getTime());
+        const response = await fetch('/assets/config/colors.json?v=' + new Date().getTime());
         colorsConfig = await response.json();
         console.log('Loaded colors config:', colorsConfig);
     } catch (error) {
